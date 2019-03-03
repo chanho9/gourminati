bccb92bbe74dade703351e1c4053e804ce0823d7
chanyoung <park910113@gmail.com>
2019-03-03 20:28:35 +0900

    add schema

---
+++ schema.sql
@@ -0,0 +1,11 @@
+-- Initialize the database.
+-- Drop any existing data and create empty tables.
+
+DROP TABLE IF EXISTS restaurant;
+
+CREATE TABLE restaurant (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  type TEXT NOT NULL,
+  address TEXT UNIQUE NOT NULL,
+);
