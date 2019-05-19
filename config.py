2ceac76260afc37987e907a4c8d9ec6b5f6ba6fc
chanho9 <pch881125@gmail.com>
2019-05-19 22:57:09 +0900

    hotfix

--- config.py
+++ config.py
@@ -6,6 +6,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     KAKAO_API_KEY = os.environ.get('GOURMINATI_KAKAO_API_KEY') or 'you will never guess'
-    # DATA_PATH = os.environ.get('GOURMINATI_DATA_PATH')
-    DATA_PATH = 'C:\\Users\\pch88\\Desktop\\gourminati\\gourminati\\guide\\db.xlsx'
+    DATA_PATH = os.environ.get('GOURMINATI_DATA_PATH')
     DATABASE = os.path.join(basedir, 'gourminati.db')
